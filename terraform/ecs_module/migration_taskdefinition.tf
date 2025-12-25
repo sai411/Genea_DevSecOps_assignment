2e750bdea6ec3f163a90615aff4dd66cd939d250
sai411 <66815779+sai411@users.noreply.github.com>
2025-12-25 23:43:15 +0530

    Create migration_taskdefinition.tf

---
+++ terraform/ecs_module/migration_taskdefinition.tf
@@ -0,0 +1,58 @@
+resource "aws_ecs_task_definition" "service" {
+  family                   = "genea-bd-migration"
+  requires_compatibilities = ["EC2"]
+  network_mode             = "awsvpc"
+  cpu                      = "256"
+  memory                   = "256"
+
+  execution_role_arn = aws_iam_role.ecs_execution_role.arn
+  task_role_arn      = aws_iam_role.ecs_task_role.arn
+
+  container_definitions = jsonencode([
+    {
+      name      = "genea-bd-migration"
+      image     = var.container_DB_image
+      essential = true
+
+      portMappings = [
+        {
+          containerPort = 80
+          hostPort      = 80
+          protocol      = "tcp"
+        }
+      ]
+
+      environment = [
+        {
+          name = "DB_NAME", value = "devdb"
+        },
+        {
+          name = "DB_HOST", value = var.dbhost
+        },
+        {
+          name = "DB_PORT", value = "3306"
+        },
+        {
+          name = "DB_USER", value = var.dbuser
+        }
+      ]
+
+      secrets = [
+        {
+          name      = "DB_PASSWORD"
+          valueFrom = "${var.db_password}:password::"
+        }
+      ]
+
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          awslogs-group         = "genea-bd-migration-ecs-logs"
+          awslogs-region        = "us-east-1"
+          awslogs-stream-prefix = "ecs"
+          awslogs-create-group  = "true"
+        }
+      }
+    }
+  ])
+}
