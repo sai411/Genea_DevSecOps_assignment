78ee2b548412b57b0efff51c3dc6fcc2ba1b2b55
sai411 <66815779+sai411@users.noreply.github.com>
2025-12-26 10:28:51 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,16 +1,4 @@
 variable "aws_region" {
   type = string
   default = "us-east-1"
-
-}
-variable "dbhost" {
-   type = string
-}
-
-variable dbuser{
-   type = string
-}
-
-variable db_password{
-   type = string
 }
